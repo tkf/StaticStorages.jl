d95351182b16b6ba78c19be9e01f7270908b149f
Takafumi Arakaki <aka.tkf@gmail.com>
2021-05-19 14:51:47 -0400

    Fix for older Julia

--- src/StaticStorages.jl
+++ src/StaticStorages.jl
@@ -16,7 +16,15 @@ function getbucket end
 
 module Implementations
 
-using UUIDs: UUID, uuid4
+using UUIDs: UUID
+
+if VERSION >= v"1.6"
+    using UUIDs: uuid4
+else
+    using Random: RandomDevice
+    using UUIDs: UUIDs
+    uuid4() = UUIDs.uuid4(RandomDevice())
+end
 
 using ..StaticStorages: StaticStorages, BucketKey, StorageKey
 
